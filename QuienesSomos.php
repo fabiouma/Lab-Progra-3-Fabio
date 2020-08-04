711b435c35e0402c1e6f89bf4442506562514823
fabiouma <65681360+fabiouma@users.noreply.github.com>
2020-08-04 12:14:13 -0600

    Add files via upload

---
+++ QuienesSomos.php
@@ -0,0 +1,117 @@
+
+<?php
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quienes somos</title>
+    <link rel="stylesheet" href="/Navegators/Normalize.css">
+    <link rel="stylesheet" href="EstilosT/Somos.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="/QuienesSomos/PaginaEstilo/QuienesSomosEstilo.css">
+<script src="https://kit.fontawesome.com/e3e2dc979f.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div>
+        <div>
+            <img src="Imagen/LogoEnterprise.PNG" class="TamañoLogo" alt="">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light" id="navar">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+
+
+                        <li class="nav-item active">
+                            <a class="nav-link" href=""><i class="fas fa-car-side"></i>
+                                QuickerFGC||Services<span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" id="Color1" href="index.php"><i class="fas fa-home"></i> Inicio<span
+                                    class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" class="Color"
+                                href="QuienesSomos.php"><i class="fas fa-users"></i>
+                                Quienes Somos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" class="Color" href="proyectosHechos.php"><i class="fas fa-hammer"></i> Proyectos
+                                Realizados</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link able" href="Consultas.php"><i
+                                    class="fas fa-mobile-alt"></i> Contactenos</a>
+                        </li>
+                        <li class="nav-item " class="Color">
+
+                        </li>
+
+                    </ul>
+                    <nav class="navbar navbar-light bg-light">
+                        <form class="form-inline">
+                            <input class="form-control mr-sm-3" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-success my-2 my-sm-0" id="Color2"
+                                type="submit">Search</button>
+                        </form>
+                    </nav>
+                </div>
+            </nav>
+        </div>
+
+
+        <div class="right">
+            <h1>Estamos presentes para darte tarnsporte a destinos deseados dentro y fuera tu area
+                con la seguridad necesaria para que llegues con la mayor eficacia.
+            </h1>
+
+            <p class="p">
+                En algún momento de nuestro tiempo hemos necesitado de algún transporte urgentemente
+                y no encontramos niguno de los convencionales
+            </p>
+
+            <b class="p">
+                No llegues tarde por falta de transporte
+            </b>
+
+            <p class="p">
+                Con nuestros servicios de vehiculos de 4 puertas podrás pedir transporte
+                en la zona donde te encuentres ubicado(a) para poder llegar a tu destino facilmente
+                con nuestra simple aplicación , no vuelvas a llegar tarde y pidenos ahora, donde sea
+                y cuando sea.
+            </p>
+
+            <b class="p">
+                Mision
+            </b>
+            <p class="p">
+                Poner a su disposición un servicio exclusivo, profesional, seguro, puntual y confiable
+                a través de un sistema en el que garantizamos una experiencia de calidad a nuestros clientes.
+            </p>
+            <b class="p">
+                Vision
+            </b>
+            <p class="p">
+                Crecer como empresa en el servicio de transporte privado, mejorando constantemente nuestros servicios,
+                para ofrecer mejor calidad y seguridad a nuestros clientes.
+            </p>
+        </div>
+    </div>
+</body>
+
+</html>
